63c82eb12977e847c5106c4995329493d5f02532
FutureSaiyajin <futuresaiyajin@online.de>
2020-02-06 17:08:15 +0100

    Create pingtrace.py

---
+++ pingtrace.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+
+commands = ['ping', 'tracert']
+network_address = input("Please enter network adress: ")
+output_dir = str(Path.home()) + "\Downloads"
+
+for command in commands:
+    os.system("powershell \"" + command + " " + network_address + " | tee " + output_dir + "\\" + command + "_" + network_address + ".txt\"")
