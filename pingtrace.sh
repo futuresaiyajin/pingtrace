9a5a218dc936e195c53be20bec79bacc92de005b
FutureSaiyajin <futuresaiyajin@online.de>
2020-02-07 21:51:04 +0100

    Update pingtrace.sh

--- pingtrace.sh
+++ pingtrace.sh
@@ -4,5 +4,5 @@ echo "Please enter network adress:"
 read NETWORK_ADDRESS
 OUTPUT_DIR=~/Downloads
 
-ping -c 4 $NETWORK_ADDRESS | tee $OUTPUT_DIR/ping_$NETWORK_ADDRESS.txt
+ping -c 4 $NETWORK_ADDRESS | tee $OUTPUT_DIR/ping_$NETWORK_ADDRESS.txt \
 && traceroute $NETWORK_ADDRESS | tee $OUTPUT_DIR/tracert_$NETWORK_ADDRESS.txt
