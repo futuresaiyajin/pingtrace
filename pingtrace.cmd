946433d08a072e4fa7be3320df4189eb1fdf2404
FutureSaiyajin <futuresaiyajin@online.de>
2020-02-18 19:57:53 +0100

    create pingtracex to outsource screenshot function

--- pingtrace.cmd
+++ pingtrace.cmd
@@ -4,4 +4,3 @@ set /p NETWORK_ADDRESS=Please enter network adress:
 set OUTPUT_DIR=%userprofile%\Downloads
 
 for %%a in ("ping" "tracert") do powershell "%%a %NETWORK_ADDRESS% | tee %OUTPUT_DIR%\\%%a_%NETWORK_ADDRESS%.txt"
-sharex -workflow "Capture active window"
---
+++ pingtracex.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+set /p NETWORK_ADDRESS=Please enter network adress: 
+set OUTPUT_DIR=%userprofile%\Downloads
+
+for %%a in ("ping" "tracert") do powershell "%%a %NETWORK_ADDRESS% | tee %OUTPUT_DIR%\\%%a_%NETWORK_ADDRESS%.txt"
+sharex -workflow "Capture active window"
